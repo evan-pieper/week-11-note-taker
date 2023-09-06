2260a39dc983c5a897c264ea75aacdb8fda45f6f
evan-pieper <evanjpieper@gmail.com>
2023-09-05 19:49:16 -0700

    implemented delete function for api

--- routes/api.js
+++ routes/api.js
@@ -23,8 +23,41 @@ api.post('/notes', (req, res) => { //posts new note to db.json from the request 
     res.end();
 });
 
-api.delete('/notes', (req, res) => {
+api.delete('/notes/:id', (req, res) => {
+    console.log('api delete notes');
+    const id = req.params.id;
+    console.log("id for clicked element: " + id);
+    const pathName = path.join(__dirname, '../db/db.json');
+    let notes = -1;
+    readFromFile(pathName).then((data) => {
+        notes = JSON.parse(data);
+        console.log(notes);
+        const newNoteArray = notes.filter(note => note.id != id);
+        console.log(newNoteArray);
+        writeToFile(pathName, newNoteArray);
+        //readAndAppend(newNoteArray, pathName);
+        res.end();
+        //res.json(newNoteArray);
+    });
+    //console.log(notes);
+    //const noteArray = JSON.parse(notes);
+    //console.log(noteArray);
+    //console.log(notes);
+    //console.log(notes.length);
+    //const newNoteArray = notes.filter(note => note.id != id);
+    //console.log(newNoteArray);
+    //writeToFile(pathName, newNoteArray);
+    //res.json(newNoteArray);
+    res.end();
     //res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
+/*const deleteNote = (id) =>
+  fetch(`/api/notes/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });*/
 
 module.exports = api;
